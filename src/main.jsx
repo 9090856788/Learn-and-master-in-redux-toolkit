a5909e98c943de90abe7eb9dc7593a661588aead
Kanhu@143 <kanhucharansahoo595@gmail.com>
2024-09-11 16:25:04 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(<App />);
